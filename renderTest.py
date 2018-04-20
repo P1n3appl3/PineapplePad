e251e59c95b66347a26d512b11166f3fffdfcc11
Joseph Ryan <josephryan3.14@gmail.com>
2018-04-20 01:17:58 -0500

    Wireframe cubes

--- renderTest.py
+++ renderTest.py
@@ -3,6 +3,8 @@ import math
 import time
 import random
 
+print "yo"
+
 fps = 30
 clk = pygame.time.Clock()
 BLACK = (0,   0,   0)
@@ -12,23 +14,7 @@ GREEN = (0, 255,   0)
 RED = (255,   0,   0)
 
 
-class vec3():
-    def __init__(self, x, y, z):
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
-
-# s is size a is angle
-    def project(self, s, a):
-        return \
-            vec2(s.x / 2 + s.x * self.x / (s.x + 2 * self.z * math.tan(a.x)),
-                 s.y / 2 + s.y * self.y / (s.y + 2 * self.z * math.tan(a.y)))
-
-    def __str__(self):
-        return "<" + str(self.x) + "," + str(self.y) + "," + str(self.z) + ">"
-
-
-class vec2():
+class v2():
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
@@ -37,31 +23,25 @@ class vec2():
         return "<" + str(self.x) + "," + str(self.y) + ">"
 
 
-class vertex():
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
+class v3():
+    def __init__(self, x, y, z):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+# s is size of viewing plane a is angle of fov
+    def project(self, s, a):
+        return \
+            v2(s.x / 2 + s.x * self.x / (s.x + 2 * self.z * math.tan(a.x)),
+               s.y / 2 + s.y * self.y / (s.y + 2 * self.z * math.tan(a.y)))
+
+    def __str__(self):
+        return "<" + str(self.x) + "," + str(self.y) + "," + str(self.z) + ">"
 
 
-class face():
-    def __init__(self, vertexes):
-        self.points = vertexes
-
-
-class cube():
-    def __init__(self, pos):
-        self.pos = pos
-
-    def getPoints(self):
-        return (self.pos, vec3(self.pos.x + 1, self.pos.y, self.pos.z),
-                vec3(self.pos.x + 1, self.pos.y + 1, self.pos.z),
-                vec3(self.pos.x, self.pos.y + 1, self.pos.z),
-                vec3(self.pos.x, self.pos.y + 1, self.pos.z + 1),
-                vec3(self.pos.x + 1, self.pos.y, self.pos.z + 1))
-
-
-size = vec2(640, 512)
-angle = vec2(math.radians(30), math.radians(20))
+size = v2(640, 512)
+#  size = v2(160, 128)
+angle = v2(math.radians(30), math.radians(20))
 
 pygame.init()
 pygame.display.set_caption("Cubefield Graphics Testing")
@@ -73,13 +53,131 @@ def drawPixel(p, color=WHITE):
     screen.set_at((int(p.x), int(p.y)), color)
 
 
-def drawLine(p1, p2, color=WHITE):
-    pygame.draw.line(
-        screen, color, (int(
-            p1.x), int(
-            p1.y)), (int(
-                p2.x), int(
-                    p2.y)), 1)
+class vertex():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, color=WHITE):
+        pygame.draw.line(
+            screen, color, (int(
+                self.p1.x), int(
+                self.p1.y)), (int(
+                    self.p2.x), int(
+                    self.p2.y)), 1)
+
+
+class cube():
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.computePoints()
+
+    def computePoints(self):
+        self.p000 = self.pos
+        self.p001 = v3(
+            self.pos.x,
+            self.pos.y,
+            self.pos.z +
+            self.size)
+        self.p010 = v3(
+            self.pos.x,
+            self.pos.y +
+            self.size,
+            self.pos.z)
+        self.p011 = v3(
+            self.pos.x,
+            self.pos.y +
+            self.size,
+            self.pos.z +
+            self.size)
+        self.p100 = v3(
+            self.pos.x +
+            self.size,
+            self.pos.y,
+            self.pos.z)
+        self.p101 = v3(
+            self.pos.x +
+            self.size,
+            self.pos.y,
+            self.pos.z +
+            self.size)
+        self.p110 = v3(
+            self.pos.x +
+            self.size,
+            self.pos.y +
+            self.size,
+            self.pos.z)
+        self.p111 = v3(
+            self.pos.x +
+            self.size,
+            self.pos.y +
+            self.size,
+            self.pos.z +
+            self.size)
+
+    def renderWireframe(self):
+        self.computePoints()
+        topLeft = vertex(
+            self.p010.project(
+                size, angle), self.p011.project(
+                size, angle))
+        topRight = vertex(
+            self.p110.project(
+                size, angle), self.p111.project(
+                size, angle))
+        bottomLeft = vertex(
+            self.p000.project(
+                size, angle), self.p001.project(
+                size, angle))
+        bottomRight = vertex(
+            self.p100.project(
+                size, angle), self.p101.project(
+                size, angle))
+        topFront = vertex(
+            self.p010.project(
+                size, angle), self.p110.project(
+                size, angle))
+        topBack = vertex(
+            self.p011.project(
+                size, angle), self.p111.project(
+                size, angle))
+        bottomFront = vertex(
+            self.p000.project(
+                size, angle), self.p100.project(
+                size, angle))
+        bottomBack = vertex(
+            self.p001.project(
+                size, angle), self.p101.project(
+                size, angle))
+        frontLeft = vertex(
+            self.p000.project(
+                size, angle), self.p010.project(
+                size, angle))
+        frontRight = vertex(
+            self.p100.project(
+                size, angle), self.p110.project(
+                size, angle))
+        backLeft = vertex(
+            self.p001.project(
+                size, angle), self.p011.project(
+                size, angle))
+        backRight = vertex(
+            self.p101.project(
+                size, angle), self.p111.project(
+                size, angle))
+        topLeft.draw()
+        topRight.draw()
+        bottomLeft.draw()
+        bottomRight.draw()
+        topFront.draw()
+        topBack.draw()
+        bottomFront.draw()
+        bottomBack.draw()
+        frontLeft.draw()
+        frontRight.draw()
+        backLeft.draw()
+        backRight.draw()
 
 
 def drawTri(p1, p2, p3, color=WHITE):
@@ -90,17 +188,13 @@ def drawQuad(p1, p2, p3, p4, color=WHITE):
     pass
 
 
-numP = 10
-points = [[[0 for x in range(numP)]for y in range(numP)]for z in range(numP)]
-proj = [[[0 for x in range(numP)]for y in range(numP)]for z in range(numP)]
-for x in range(numP):
-    for y in range(numP):
-        for z in range(numP):
-            off = random.random() * 10 - 5
-            points[x][y][z] = vec3(
-                x * 100 + off - 500,
-                y * 100 + off - 500,
-                z * 100 + off)
+numCubes = 20
+cubeSize = 50
+speed = 5
+cubes = []
+for i in range(numCubes):
+    cubes.append(cube(v3(random.random() * 800 - 400,
+                         100, i * 1000 / numCubes), cubeSize))
 
 while running:
     screen.fill(BLACK)
@@ -113,47 +207,27 @@ while running:
         running = False
 
     if(keys[pygame.K_a]):
-        for i in points:
-            for j in i:
-                for p in j:
-                    p.x += 10
+        for c in cubes:
+            c.pos.x += speed
 
     if(keys[pygame.K_d]):
-        for i in points:
-            for j in i:
-                for p in j:
-                    p.x -= 10
-
-    if(keys[pygame.K_w]):
-        for i in points:
-            for j in i:
-                for p in j:
-                    p.y += 10
+        for c in cubes:
+            c.pos.x -= speed
 
     if(keys[pygame.K_s]):
-        for i in points:
-            for j in i:
-                for p in j:
-                    p.y -= 10
+        for c in cubes:
+            c.pos.y -= speed
 
-    for i in range(numP):
-        for j in range(numP):
-            for k in range(numP):
-                p = points[i][j][k]
-                p.z -= 5
-                if p.z < -10:
-                    p.z = 1000
-                projection = p.project(size, angle)
-                proj[i][j][k] = projection
-                if size.x >= projection.x >= 0 and size.y >= projection.y >= 0:
-                    #  drawPixel(projection, 3 * (250 - p.z / 4,))
-                    pass
-    for i in range(numP - 1):
-        for j in range(numP - 1):
-            for k in range(numP - 1):
-                drawLine(proj[i][j][k], proj[i + 1][j][k])
-                drawLine(proj[i][j][k], proj[i][j + 1][k])
-                drawLine(proj[i][j][k], proj[i][j][k + 1])
+    if(keys[pygame.K_w]):
+        for c in cubes:
+            c.pos.y += speed
+
+    for c in cubes:
+        c.pos.z -= speed
+        if c.pos.z < -30:
+            c.pos.z = 1000
+        c.renderWireframe()
+
     pygame.transform.flip(screen, False, False)
     pygame.display.flip()
     clk.tick(fps)
